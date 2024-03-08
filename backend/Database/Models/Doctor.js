34ddfe4dd7de875e9667639d329feeb290fc6073
destryptor <sharanya.chakraborty@kgpian.iitkgp.ac.in>
2024-03-08 13:05:01 +0530

    Login form created

--- backend/Database/Models/Doctor.js
+++ backend/Database/Models/Doctor.js
@@ -3,13 +3,13 @@ const mongoose = require('mongoose');
 const doctorSchema = new mongoose.Schema(
 	{
 		_id: mongoose.Schema.Types.ObjectId,
-		name: { type: String, required: true },
+		name: { type: String, required: true, default: '' },
 		email: { type: String, required: true },
 		password: { type: String, required: true },
-		specialisation: { type: String, required: true },
-		certification: { type: String, required: true },
-		location: { type: String, required: true },
-		clinic: { type: String, required: true },
+		specialisation: { type: String, required: true, default: '' },
+		certification: { type: String, required: true, default: '' },
+		location: { type: String, required: true, default: '' },
+		clinic: { type: String, required: true, default: '' },
 		patients: [
 			{
 				type: mongoose.Schema.Types.ObjectId,
--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import Login from './Pages/Login';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
-	return <div className='App'></div>;
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path='/' element={<Login />}>
+					{/* <Route index element={<Home />} />
+					<Route path='blogs' element={<Blogs />} />
+					<Route path='contact' element={<Contact />} />
+					<Route path='*' element={<NoPage />} /> */}
+				</Route>
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
