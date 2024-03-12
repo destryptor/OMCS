225ff96711355170714eda1ca370cdc8d4ba90d6
Jatin Mahawar <jatinmahawar08@kgpian.iitkgp.ac.in>
2024-03-12 21:47:54 +0530

    Navar fixed

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -157,7 +157,7 @@ const Login = ({ isDoctor }) => {
 		<>
 			<Toaster />
 			{isLogin ? (
-				<div className='flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8'>
+				<div className='flex min-h-full flex-1 flex-col justify-center px-6 py-24 lg:px-8 '>
 					<div className='sm:mx-auto sm:w-full sm:max-w-sm'>
 						<img className='mx-auto h-10 w-auto' src='./logo.png' alt='Your Company' />
 						<h2 className='mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900'>{isDoctor ? 'Doctor Login' : 'Patient Login'}</h2>
@@ -201,7 +201,7 @@ const Login = ({ isDoctor }) => {
 					</div>
 				</div>
 			) : (
-				<div className='flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8'>
+				<div className='flex min-h-full flex-1 flex-col justify-center px-6 py-24 lg:px-8'>
 					<div className='sm:mx-auto sm:w-full sm:max-w-sm'>
 						<img className='mx-auto h-10 w-auto' src='./logo.png' alt='Your Company' />
 						<h2 className='mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900'>{isDoctor ? 'Doctor Sign Up' : 'Patient Sign Up'}</h2>
--- src/Pages/UpdateProfile.jsx
+++ src/Pages/UpdateProfile.jsx
@@ -193,7 +193,7 @@ function UpdateProfile() {
 		<>
 			<Toaster />
 			{isDoctor ? (
-				<div className='flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8'>
+				<div className='flex min-h-full flex-1 flex-col justify-center px-6 py-24 lg:px-8'>
 					<div className='sm:mx-auto sm:w-full sm:max-w-sm'>
 						<img className='mx-auto h-10 w-auto' src='/logo.png' alt='OMCS' />
 						<h2 className='mt-1 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900'>Update Your Profile</h2>
@@ -285,7 +285,7 @@ function UpdateProfile() {
 					</div>
 				</div>
 			) : (
-				<div className='flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8'>
+				<div className='flex min-h-full flex-1 flex-col justify-center px-6 py-24 lg:px-8'>
 					<div className='sm:mx-auto sm:w-full sm:max-w-sm'>
 						<img className='mx-auto h-10 w-auto' src='/logo.png' alt='Your Company' />
 						<h2 className='mt-1 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900'>Update Your Profile</h2>
