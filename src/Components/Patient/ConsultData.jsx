d7cd96d911d7c49f89e5e3efbb775818ae001105
Jatin Mahawar <jatinmahawar08@kgpian.iitkgp.ac.in>
2024-03-15 11:01:34 +0530

    File Structure Changed

--- src/Components/Patient/ConsultData.jsx
+++ src/Components/Patient/ConsultData.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import toast, { Toaster } from "react-hot-toast";
-import ConsultCard from "../Components/ConsultCard";
+import ConsultCard from "../Patient/ConsultCard";
 import Data from './Data'
 function ConsultData({ location }) {
   return (
