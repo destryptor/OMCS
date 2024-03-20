fc768f1ad5536b84480ae0cacf229605e5d9e274
shivachaits <143306775+shivachaits@users.noreply.github.com>
2024-03-20 19:10:01 +0530

    Handled delete,completed and added reject

--- backend/Database/Models/Patient.js
+++ backend/Database/Models/Patient.js
@@ -1,22 +1,26 @@
 const mongoose = require('mongoose');
 
 const patientSchema = new mongoose.Schema(
-	{
-		name: { type: String, default: '' },
-		email: { type: String },
-		password: { type: String },
-		age: { type: Number, default: '' },
-		location: { type: String, default: '' },
-		symptoms: { type: String, default: '' },
-		doctor: [
-			{
-				email: { type: String, required: true },
-				status: { type: String, default: 'Pending' },
-				symptoms: { type: String, default: '' },
-			},
-		],
-	},
-	{ collection: 'patient' }
+  {
+    name: { type: String, default: "" },
+    email: { type: String },
+    password: { type: String },
+    age: { type: Number, default: "" },
+    location: { type: String, default: "" },
+    symptoms: { type: String, default: "" },
+    doctor: [
+      {
+        email: { type: String, required: true },
+        status: { type: String, default: "Pending" },
+        symptoms: { type: String, default: "" },
+        id: {
+          type: mongoose.Schema.Types.ObjectId,
+          required: true,
+        },
+      },
+    ],
+  },
+  { collection: "patient" }
 );
 
 module.exports = mongoose.model('Patient', patientSchema);
